e566fdefe7ceaa314d0db9671a7537de0ff78ac5
Paige Hetherington <herbmama@gmail.com>
2016-03-30 10:43:35 -0400

    displays window

--- src/ants_clojure/core.clj
+++ src/ants_clojure/core.clj
@@ -1,5 +1,17 @@
-(ns ants-clojure.core
-  (:gen-class))
+(ns ants-clojure.core 
+  (:require [clojure.java.io :as io])
+  (:import [javafx.application Application] 
+           [javafx.fxml FXMLLoader]
+           [javafx.scene Scene])
+  (:gen-class :extends javafx.application.Application))
+
+(defn -start [app stage]
+  (let [root (FXMLLoader/load (io/resource "main.fxml")) ;reads fxml file from resources and gives to loader
+        scene (Scene. root 800 600)] ; to instatantiate java class in clojure scene.
+    (.setTitle stage "Ants")
+    (.setScene stage scene)
+    (.show stage)))
+             
 
 (defn -main []
-  (println "Hello, World!"))
+  (Application/launch ants_clojure.core (into-array String []))) ;initializes javafx
